f25b997028151c01a3d574b11527318fe5890127
dungvan2512 <dungvan2512@gmail.com>
2018-05-12 09:18:47 +0700

    fix inFolder team model

--- team/team_model.go
+++ team/team_model.go
@@ -2,9 +2,10 @@ package team
 
 // Player truct
 type Player struct {
-	ID       uint   `json:"id"`
-	UserName string `json:"user_name"`
-	FullName string `json:"full_name"`
-	Score    uint   `json:"score"`
-	Position string `json:"position"`
+	ID        uint   `json:"id"`
+	UserName  string `json:"user_name"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Score     uint   `json:"score"`
+	Position  string `json:"position"`
 }
