d5debeafd41b615550f2e36d7d97bfc63a57ea27
dungvan2512 <dungvan2512@gmail.com>
2018-05-30 01:19:12 +0700

    ad s3 storage and reconfig regex

--- shared/validator/regexes.go
+++ shared/validator/regexes.go
@@ -4,8 +4,8 @@ import "regexp"
 
 const (
 	hashtagRegexString   = "^[Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ゖゝ-ゟァ-ヺー-ヿㇰ-ㇿｦ-ﾝー_]+$"
-	imageNameRegexString = "^[a-zA-Z0-9_\\._:-]{1,256}(\\?w=[0-9]{1,5}\\&h=[0-9]{1,5})?$"
-	videoNameRegexString = "^[a-zA-Z0-9_\\._:-]{1,256}(\\?w=[0-9]{1,5}\\&h=[0-9]{1,5})?$"
+	imageNameRegexString = "^[a-zA-Z0-9_\\/._:-]{1,256}(\\?w=[0-9]{1,5}\\&h=[0-9]{1,5})?$"
+	videoNameRegexString = "^[a-zA-Z0-9_\\/._:-]{1,256}(\\?w=[0-9]{1,5}\\&h=[0-9]{1,5})?$"
 )
 
 var (
